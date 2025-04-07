692d91640869d28aff493697ee1a01bac415facf
Alok Singh <Alok.Singh@teknopoint.local>
2025-04-07 11:46:05 +0530

    code add

--- blocks/hero-banner/hero-banner.js
+++ blocks/hero-banner/hero-banner.js
@@ -1,18 +1,15 @@
 export default function decorate(block) {
     Array.from(block.children).forEach((row) => {
-      row.classList.add("hero-banner-ctnr");
-  
-      Array.from(row.children).forEach((column, index) => {
-        column.classList.add("hero-banner-ctnr-col");
-        column.classList.add(`sec-${index + 1}`);
-  
-        // Add custom class to all children inside sec-2
-        if (column.classList.contains('sec-2')) {
-          Array.from(column.children).forEach((child, i) => {
-            child.classList.add(`sec-2-text-${i + 1}`);
-          });
-        }
-      });
+        row.classList.add("hero-banner-ctnr");
+
+        Array.from(row.children).forEach((column, index) => {
+            column.classList.add("hero-banner-ctnr-col");
+            column.classList.add(`sec-${index + 1}`);
+            if (column.classList.contains('sec-2')) {
+                Array.from(column.children).forEach((child, i) => {
+                    child.classList.add(`sec-2-text-${i + 1}`);
+                });
+            }
+        });
     });
-  }
-  
+}
