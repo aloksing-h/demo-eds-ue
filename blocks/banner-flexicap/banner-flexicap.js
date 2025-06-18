ffcebdfba840af754d79f2f6123258f9193787af
Alok Singh <Alok.Singh@teknopoint.local>
2025-06-18 11:41:47 +0530

    fexicap code addx

--- blocks/banner-flexicap/banner-flexicap.js
+++ blocks/banner-flexicap/banner-flexicap.js
@@ -1,22 +1,22 @@
 // import Swiper from "../swiper-sec/swiper-bundle.min.js";
 // import swiperBundleMin from "../swiper-sec/swiper-bundle.min.js";
 
-export default function decorate(block) {
-    Array.from(block.children).forEach((row, rowIndex) => {
-        row.classList.add("flexicap-fund");
-        row.classList.add(`sec-${rowIndex + 1}`);
-        // Array.from(row.children).forEach((column, colIndex) => {
-        //     column.classList.add("flexicap-fund-column");
-        //     column.classList.add(`sebtxt-${colIndex + 1}`);
-        // });
-    });
+// export default function decorate(block) {
+//     Array.from(block.children).forEach((row, rowIndex) => {
+//         row.classList.add("flexicap-fund");
+//         row.classList.add(`sec-${rowIndex + 1}`);
+//         // Array.from(row.children).forEach((column, colIndex) => {
+//         //     column.classList.add("flexicap-fund-column");
+//         //     column.classList.add(`sebtxt-${colIndex + 1}`);
+//         // });
+//     });
 
-    // const listBlocks = block.querySelectorAll(".flexicap-fund-column ul");
-    // listBlocks.forEach((ul, index) => {
-    //     ul.classList.add(`flexicap-fund-ul-${index + 1}`);
-    // });
-    const sectional = block.querySelector(".flexicap-fund.sec-2 .button-container>div>div");
-    if (sectional) {
-        sectional.classList.add('new-ad');
-    }
-}
+//     // const listBlocks = block.querySelectorAll(".flexicap-fund-column ul");
+//     // listBlocks.forEach((ul, index) => {
+//     //     ul.classList.add(`flexicap-fund-ul-${index + 1}`);
+//     // });
+//     const sectional = block.querySelector(".flexicap-fund.sec-2 .button-container>div>div");
+//     if (sectional) {
+//         sectional.classList.add('new-ad');
+//     }
+// }
